05e25c43c4e763d35905f769185dc9a7fbf682e6
jackhcarpenter <carpenterj3@cardiff.ac.uk>
2023-10-11 16:35:59 +0100

    debugging 0.3-fastp.sh
    fixing syntax errors

--- 0.3-fastp.sh
+++ 0.3-fastp.sh
@@ -41,33 +41,33 @@ export workingdir=/mnt/scratch/c1831460/RNA-seq
 
 # Loop variables
 
-lane=("L001" \ 
+lane=("L001" \
 	"L002")
 
-list=("STM_C3_S7" \ 
-	"STM_C4_S8" \ 
-	"STM_C5_S9" \ 
-	"STM_CD3_S10" \ 
-	"STM_CD4_S11" \ 
-	"STM_CD5_S12" \ 
-	"STM_D3_S4" \ 
-	"STM_D4_S5" \ 
-	"STM_D5_S6" \ 
-	"STM_M3_S1" \ 
-	"STM_M4_S2" \ 
-	"STM_M5_S3" \ 
-	"TCP_C2_S19" \ 
-	"TCP_C4_S20" \ 
-	"TCP_C5_S21" \ 
-	"TCP_CO2_S22" \ 
-	"TCP_CO4_S23" \ 
-	"TCP_CO5_S24" \ 
-	"TCP_D2_S16" \ 
-	"TCP_D4_S17" \ 
-	"TCP_D5_S18" \ 
-	"TCP_M2_S13" \ 
-	"TCP_M4_S14" \ 
-	"TCP_M5_S15" \ 
+list=("STM_C3_S7" \
+	"STM_C4_S8" \
+	"STM_C5_S9" \
+	"STM_CD3_S10" \
+	"STM_CD4_S11" \
+	"STM_CD5_S12" \
+	"STM_D3_S4" \
+	"STM_D4_S5" \
+	"STM_D5_S6" \
+	"STM_M3_S1" \
+	"STM_M4_S2" \
+	"STM_M5_S3" \
+	"TCP_C2_S19" \
+	"TCP_C4_S20" \
+	"TCP_C5_S21" \
+	"TCP_CO2_S22" \
+	"TCP_CO4_S23" \
+	"TCP_CO5_S24" \
+	"TCP_D2_S16" \
+	"TCP_D4_S17" \
+	"TCP_D5_S18" \
+	"TCP_M2_S13" \
+	"TCP_M4_S14" \
+	"TCP_M5_S15" \
 	"Undetermined_S0")
 
 # Trim low quality reads, remove adapters, and poly Gs
@@ -82,7 +82,7 @@ do
 
         fastp \
             -i $workingdir/${i}_${x}.R1.fastq.gz \
-            -I $workingdir/${i}_${x}.R2.fastq.gz \
+			-I $workingdir/${i}_${x}.R2.fastq.gz \
             --detect_adapted_for_pe \
             --trim_poly_g \
             --correction \
