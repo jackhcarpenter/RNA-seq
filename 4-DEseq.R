0116133e595f9852864de0a856af86527d58f9d6
jackhcarpenter <carpenterj3@cardiff.ac.uk>
2023-11-01 15:15:25 +0000

    Adapting 4-DEseq.R for use

--- 4-DEseq.R
+++ 4-DEseq.R
@@ -16,24 +16,104 @@ library("DESeq2")
 
 # Point to the working dir
 
-swd("path / to / your / dir")
+setwd("C:/Users/c1831460/OneDrive - Cardiff University/Documents/DTP/Second Year/RNA-seq/featureCounts/")
 
 # Create dataframe contianing output from 3-featureCounts.sh
 
-rmdup <- list.files(
-  pattern = " * rmdup.featurecount")
+markdup <- list.files(
+  pattern = " *.markdup.featurecounts.csv")
 
+rmdup <- list.files(
+  pattern = " *.rmdup.featurecounts.csv")
 
 # Create tables to tell DESeq2 what the variables are
 # The number of samplesheets (coldata) you need will depend on the number of
-# timepoints/
-# variables you want to compare
+# timepoints/variables you want to compare
 
+
+#All
 coldata <- data.frame(
-  sample = c("sample_1", "sample_2", "sample_n"),
-  genotype = c("genotype_1", "genotype_2", "genotype_n"),
-  replicate = c("replicate_1", "replicate_2", "replicate_n")
+                      sample = c("STM_C3_S7",
+                                 "STM_C4_S8",
+                                 "STM_C5_S9",
+                                 "STM_CD3_S10",
+                                 "STM_CD4_S11",
+                                 "STM_CD5_S12",
+                                 "STM_D3_S4",
+                                 "STM_D4_S5",
+                                 "STM_D5_S6",
+                                 "STM_M3_S1",
+                                 "STM_M4_S2",
+                                 "STM_M5_S3",
+                                 "TCP_C2_S19",
+                                 "TCP_C4_S20",
+                                 "TCP_C5_S21",
+                                 "TCP_CO2_S22",
+                                 "TCP_CO4_S23",
+                                 "TCP_CO5_S24",
+                                 "TCP_D2_S16",
+                                 "TCP_D4_S17",
+                                 "TCP_D5_S18",
+                                 "TCP_M2_S13",
+                                 "TCP_M4_S14",
+                                 "TCP_M5_S15"
+                                 ),
+                      genotype = c("C", "C", "C",
+                                   "CD","CD","CD",
+                                   "D","D","D",
+                                   "M","M", "M"
+                                   ),
+                      replicate = c("1", "2", "3"
+                                    )
 )
+
+#STM
+coldata <- data.frame(
+                      sample = c("STM_C3_S7",
+                                 "STM_C4_S8",
+                                 "STM_C5_S9",
+                                 "STM_CD3_S10",
+                                 "STM_CD4_S11",
+                                 "STM_CD5_S12",
+                                 "STM_D3_S4",
+                                 "STM_D4_S5",
+                                 "STM_D5_S6",
+                                 "STM_M3_S1",
+                                 "STM_M4_S2",
+                                 "STM_M5_S3"
+                                  ),
+                      genotype = c("C", "C", "C",
+                                   "CD","CD","CD",
+                                   "D","D","D",
+                                   "M","M", "M"
+                                    ),
+                      replicate = c("1", "2", "3")
+)
+
+#TCP4
+coldata <- data.frame(
+                      sample = c("TCP_C2_S19",
+                                 "TCP_C4_S20",
+                                 "TCP_C5_S21",
+                                 "TCP_CO2_S22",
+                                 "TCP_CO4_S23",
+                                 "TCP_CO5_S24",
+                                 "TCP_D2_S16",
+                                 "TCP_D4_S17",
+                                 "TCP_D5_S18",
+                                 "TCP_M2_S13",
+                                 "TCP_M4_S14",
+                                 "TCP_M5_S15"
+                                  ),
+                      genotype = c("C", "C", "C",
+                                   "CD","CD","CD",
+                                   "D","D","D",
+                                   "M","M", "M"
+                                   ),
+                      replicate = c("1", "2", "3")
+)
+
+dup = list(markdup = markdup, rmdup = rmdup)
 
 ################################################################################
 # Main CMDs
@@ -41,124 +121,132 @@ coldata <- data.frame(
 
 # Create loop
 
-for(i in rmdup) {
+namefield <- c("markdup", "rmdup")
+index = as.numeric(1)
 
-# Read the data from standard input
-
-  data <- as.matrix(read.csv(
-    file = paste (i, sep = ""),
-    header = TRUE,
-    row.names = "Geneid",
-    sep = "\t"))
+for (i in dup) {
+  
+  for(j in i) {
+  
+  # Read the data from standard input
+  
+    data <- as.matrix(read.csv(
+      file = paste (j, ".", namefield[index], ".", "featurecounts.csv", 
+                    sep = ""),
+      header = TRUE,
+      row.names = "Geneid",
+      sep = "\t"))
+      
+  # Use dds to combine the coldata and countdata matrix
+  # State the variable/factor being analysed using the "design" flag
+  
+    dds <- DESeqDataSetFromMatrix(
+      countData = data, 
+      colData = coldata,
+      design = ~ genotype)
     
-# Use dds to combine the coldata and countdata matrix
-# State the variable/factor being analysed using the "design" flag
-
-  dds <- DESeqDataSetFromMatrix(
-    countData = data, 
-    colData = coldata,
-    design = ~ genotype)
-  
-  dds
-  
-# As R will automatically choose the reference level for the variable/factor,
-# the control group needs to be defined and releveled
-
-  dds$genotype <- relevel(
-    dds$genotype, 
-    ref = "WT")
-
-# Run the DESeq command on the DESeq dataset
-
-  dds <- DESeq(dds)
-
-# Generate a results table, and specify the contrast we want to build
-
-  res <- results(
-    dds,
-    contrast=c(
-      "genotype",
-      "WT",
-      "TCP4"))
-
-#OR...
-
-  res <- results(
-    dds,
-    name = "genotype_TCP4_vs_WT")
-
-# Log fold change shrinkage for visualisation can be done using different models
-
-  resLFC <- lfcShrink(
-    dds, 
-    coef= "genotype_TCP4_vs_WT",
-    type="apeglm")
-
-  resLFC
-
-#OR...
-
-  resNorm <- lfcShrink(
-    dds, 
-    coef = "genotype_TCP4_vs_WT", 
-    type = "normal")
-  
-  resNorm
-
-# Plotting the normalised results with MA
-# Set the probability and log fold change thresholds
-
-  xlim <- c(1,1e5); ylim <- c(-3,3)
-  plotMA(
-    resLFC, 
-    xlim=xlim, 
-    ylim=ylim, 
-    main="apeglm")
-
-# The plot can be used to identify the rownumber of individual genes
-# interactively
-
-  idx <- identify(
-    res$baseMean, 
-    res$log2FoldChange)
-  
-  rownames(res)[idx]
-
-# Extract all genes (independent of differential expression)
-  
-  write.csv(resLFC, (
-    file = paste( 
-      i, 
-      "_DEGs.csv", 
-      sep = "")))
-
-# Extract significant upregulated and down regulated genes into separate
-# datasets
-
-  upreg <- subset(
-    resLFC, 
-    log2FoldChange >=0.5 & padj <0.05)
-  
-  upreg <- upreg[ , c(-1,-3)]
-
-  downreg <-subset(
-    resLFC, 
-    log2FoldChange <=-0.5 & padj <0.05)
-  
-  downreg <- downreg[ , c(-1,-3)]
-
-#write csv file of for up and down regualted gene IDs
-
-  write.csv(upreg, (
-    file = paste( 
-      i, 
-      "_upreg.csv", 
-      sep = "")))
+    dds
     
-  write.csv(downreg, (
-    file = paste( 
-      i, 
-      "_downreg.csv", 
-      sep = "")))
+  # As R will automatically choose the reference level for the variable/factor,
+  # the control group needs to be defined and releveled
   
+    dds$genotype <- relevel(
+      dds$genotype, 
+      ref = "M")
+  
+  # Run the DESeq command on the DESeq dataset
+  
+    dds <- DESeq(dds)
+  
+  # Generate a results table, and specify the contrast we want to build
+  
+    res <- results(
+      dds,
+      contrast=c(
+        "genotype",
+        "M",
+        "C"))
+  
+  #OR...
+  
+  #  res <- results(
+  #    dds,
+  #    name = "genotype_TCP4_vs_WT")
+  
+  # Log fold change shrinkage for visualisation can be done using different models
+  
+    resLFC <- lfcShrink(
+      dds, 
+      coef= "genotype_STM_M_vs_STM_C",
+      type= "apeglm")
+  
+    resLFC
+  
+  #OR...
+  
+#    resNorm <- lfcShrink(
+#      dds, 
+#      coef = "genotype__vs_", 
+#      type = "normal")
+    
+#    resNorm
+  
+  # Plotting the normalised results with MA
+  # Set the probability and log fold change thresholds
+  
+    xlim <- c(1,1e5); ylim <- c(-3,3)
+    plotMA(
+      resLFC, 
+      xlim=xlim, 
+      ylim=ylim, 
+      main="apeglm")
+  
+  # The plot can be used to identify the rownumber of individual genes
+  # interactively
+  
+    idx <- identify(
+      res$baseMean, 
+      res$log2FoldChange)
+    
+    rownames(res)[idx]
+  
+  # Extract all genes (independent of differential expression)
+    
+    write.csv(resLFC, (
+      file = paste( 
+        j, "_", namefield[index], 
+        "_DEGs.csv", 
+        sep = "")))
+  
+  # Extract significant upregulated and down regulated genes into separate
+  # datasets
+  
+    upreg <- subset(
+      resLFC, 
+      log2FoldChange >=0.5 & padj <0.05)
+    
+    upreg <- upreg[ , c(-1,-3)]
+  
+    downreg <-subset(
+      resLFC, 
+      log2FoldChange <=-0.5 & padj <0.05)
+    
+    downreg <- downreg[ , c(-1,-3)]
+  
+  #write csv file of for up and down regualted gene IDs
+  
+    write.csv(upreg, (
+      file = paste( 
+        j, ".", namefield[index],
+        "_upreg.csv", 
+        sep = "")))
+      
+    write.csv(downreg, (
+      file = paste( 
+        j, ".", namefield[index],
+        "_downreg.csv", 
+        sep = "")))
+    
+  }
+  index <- index+1
 }
