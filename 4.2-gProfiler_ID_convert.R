87f5de0a908105061286e2f782fa18e028d2778d
jackhcarpenter <carpenterj3@cardiff.ac.uk>
2024-03-21 14:03:09 +0000

    Update to 4.2-gProfiler_ID_convert.R
    Adding annotation to script and altering formatting.

--- 4.2-gProfiler_ID_convert.R
+++ 4.2-gProfiler_ID_convert.R
@@ -1,4 +1,4 @@
-###################################################################################
+################################################################################
 # Loading dependencies
 ################################################################################
 
@@ -14,16 +14,23 @@ library("gprofiler2")
 setwd("C:/Users/c1831460/OneDrive - Cardiff University/Documents/DTP/Second Year/RNA-seq/featureCounts/DEGs/")
 
 
-#files <- "STM/STM_C_vs_CD_markdup_DEGs.csv"
-files <- c(list.files("TCP4/", include.dirs = FALSE, full.names = TRUE), list.files("STM/", include.dirs = FALSE, full.names = TRUE))
+# Read in all the files
+files <- c(list.files("TCP4/", include.dirs = FALSE, full.names = TRUE),
+           list.files("STM/", include.dirs = FALSE, full.names = TRUE))
 
+# Creating subdirectories for output files - these lines should not be run before
+# line 18
 dir.create("STM/NamesAdded")
 dir.create("TCP4/NamesAdded")
 
 for (i in files) {
-  
+
+  # Read in each file as a csv  
   csvfile <- read.csv(file = paste0(i))
+  # Change the name of first column to "geneID"
   names(csvfile)[1] <- "GeneID"
+  # Extract the first/GeneID column and pass it into the query object to be read
+  # by gconvert
   IDcolumn <- as.matrix(csvfile %>% select(1))
   query <- as.character(IDcolumn)
 
@@ -35,19 +42,26 @@ for (i in files) {
                         filter_na = TRUE
                         )
   
+  # Extract the name and description columns from the output
   GeneNameColumns <- GeneNames %>% 
     select(name, description)
   
+  # Insert the name and description columns in between the first and second 
+  # columns from the initial csv
   outputdf <- bind_cols(select(csvfile, 1), 
                         GeneNameColumns, 
                         select(csvfile, -1))
   outputdf <- as.data.frame(outputdf)
   
+  # Remove parent directory (e.g. STM/) from the string held in the i variable
   i <- sub(".*/", "", i)
 
+  # Write files into appropriate subdirectories depending on genotype
   if (grepl("STM", i)) {
-    write.csv(outputdf, file = file.path("STM/NamesAdded", paste0(i, "")), row.names = FALSE)
+    write.csv(outputdf, file = file.path("STM/NamesAdded", paste0(i, "")), 
+              row.names = FALSE)
   } else {
-    write.csv(outputdf, file = file.path("TCP4/NamesAdded", paste0(i, "")), row.names = FALSE)
+    write.csv(outputdf, file = file.path("TCP4/NamesAdded", paste0(i, "")), 
+              row.names = FALSE)
   }
 }
