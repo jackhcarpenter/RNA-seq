f1d80e5bad5176c73b36a7983dc3d882979c4541
jackhcarpenter <carpenterj3@cardiff.ac.uk>
2024-06-21 11:31:37 +0100

    0.2-fastqc reusability
    Removing the list of file names at the beginning and replacing it with a wildcard in directory. Also includes an error flag in the output to notify the user that files have been rejected if they are not .fastq files.

--- 0.2-fastqc.sh
+++ 0.2-fastqc.sh
@@ -3,8 +3,8 @@
 #SBATCH --partition=defq       # the requested queue
 #SBATCH --nodes=1              # number of nodes to use
 #SBATCH --tasks-per-node=1     # for parallel distributed jobs
-#SBATCH --cpus-per-task=4      # for multi-threaded jobs
-#SBATCH --mem-per-cpu=4G      # in megabytes, unless unit explicitly stated
+#SBATCH --cpus-per-task=1      # for multi-threaded jobs
+#SBATCH --mem-per-cpu=32G      # in megabytes, unless unit explicitly stated
 #SBATCH --error=logs/%J.err         # redirect stderr to this file
 #SBATCH --output=logs/%J.out        # redirect stdout to this file
 #SBATCH --mail-user=carpenterj3@cardiff.ac.uk      # email
@@ -30,9 +30,15 @@ echo \$SLURM_MEM_PER_CPU=${SLURM_MEM_PER_CPU}
 
 module load fastqc/v0.11.9
 
-export workingdir=/mnt/scratch/xxxxxx/RNA-seq
+export workingdir=/mnt/scratch/xxxxxx/RNA-seq_TCP4_STM/fastq/merged
 
-##REMEMBER: set up any directories that the software needs in this script in case 
+echo "working dir =" $workingdir
+
+export exportdir=/mnt/scratch/xxxxxx0/RNA-seq_TCP4_STM/fastqc/
+
+echo "export dir =" $exportdir
+
+##REMEMBER: set up any directories that the software needs in this script in case
 ##it is unable to do so itself
 
 #################################################################################
@@ -41,58 +47,30 @@ export workingdir=/mnt/scratch/xxxxxx/RNA-seq
 
 # Loop variables
 
-lane=("L001" \
-	"L002")
-
-list=("STM_C3_S7" \
-	"STM_C4_S8" \
-	"STM_C5_S9" \
-	"STM_CD3_S10" \
-	"STM_CD4_S11" \
-	"STM_CD5_S12" \
-	"STM_D3_S4" \
-	"STM_D4_S5" \
-	"STM_D5_S6" \
-	"STM_M3_S1" \
-	"STM_M4_S2" \
-	"STM_M5_S3" \
-	"TCP_C2_S19" \
-	"TCP_C4_S20" \
-	"TCP_C5_S21" \
-	"TCP_CO2_S22" \
-	"TCP_CO4_S23" \
-	"TCP_CO5_S24" \
-	"TCP_D2_S16" \
-	"TCP_D4_S17" \
-	"TCP_D5_S18" \
-	"TCP_M2_S13" \
-	"TCP_M4_S14" \
-	"TCP_M5_S15" \
-	"Undetermined_S0")
-
-# fastqc of paired end data
-
-echo "============================="
-echo "RUNNING fastqc"
-
-for x in ${lane[@]}
+for file in $workingdir/*
 do
-	for i in ${list[@]}
-	do
-		echo ${i} "= running"
-		 
-		fastqc \
-			$workingdir/fastq/${i}_${x}_R1_001.fastq.gz \
-			-o $workingdir/fastqc/
-	   	fastqc \
-			$workingdir/fastq/${i}_${x}_R2_001.fastq.gz \
-			-o $workingdir/fastqc/
+        if [[ $file == *.fastq ]]
+        then
 
-	    echo ${i} "= complete"
-	done
-done
+                echo "============================="
+                echo "RUNNING fastqc"
+                echo ${file} "= running"
 
-echo "fastqc ENDE
+                fastqc \
+                        ${file} \
+                        -o $exportdir
+
+                echo ${file} "= complete"
+
+
+            else
+                    echo "============================="
+                    echo "ignoring" ${file} "-  not a fastq file"
+
+            fi
+    done
+
+echo "fastqc complete"
 echo "============================="
 
 #################################################################################
