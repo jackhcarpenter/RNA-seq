f428a16afa2eb31fd8d07ea768bcff16bf90f2d6
jackhcarpenter <carpenterj3@cardiff.ac.uk>
2024-06-26 16:32:30 +0100

    1.2-mapping.sh reusability
    Update to script to look in the fastp dir for fastp files to align

--- 1.2-mapping.sh
+++ 1.2-mapping.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=defq       # the requested queue
 #SBATCH --nodes=1              # number of nodes to use
 #SBATCH --tasks-per-node=1     #
-#SBATCH --cpus-per-task=8      #   
+#SBATCH --cpus-per-task=8      #
 #SBATCH --mem-per-cpu=4G     # in megabytes, unless unit explicitly stated
 #SBATCH --time=48:00:00
 #SBATCH --error=logs/%J.err         # redirect stderr to this file
@@ -33,13 +33,17 @@ module load STAR/2.7.6a
 
 # point to the directory containing the reference genome
 
-export refdir=/mnt/scratch/xxxxxx/RNA-seq/reference_genome
+export refdir=/mnt/scratch/xxxxxx/RNA-seq_TCP4_STM/reference_genome
 
 # define the working directory
 
-export workingdir=/mnt/scratch/xxxxxx/RNA-seq
+export workingdir=/mnt/scratch/xxxxxx/RNA-seq_TCP4_STM/fastp
 
-##REMEMBER: set up any directories that the software needs in this script in case 
+# define the export directory
+
+export exportdir=/mnt/scratch/xxxxxx/RNA-seq_TCP4_STM/STAR
+
+##REMEMBER: set up any directories that the software needs in this script in case
 ##it is unable to do so itself
 
 #################################################################################
@@ -48,60 +52,43 @@ export workingdir=/mnt/scratch/xxxxxx/RNA-seq
 
 # Loop variables
 
-lane=("L001" \
-	"L002")
+declare -a files
 
-list=("STM_C3_S7" \
-	"STM_C4_S8" \
-	"STM_C5_S9" \
-	"STM_CD3_S10" \
-	"STM_CD4_S11" \
-	"STM_CD5_S12" \
-	"STM_D3_S4" \
-	"STM_D4_S5" \
-	"STM_D5_S6" \
-	"STM_M3_S1" \
-	"STM_M4_S2" \
-	"STM_M5_S3" \
-	"TCP_C2_S19" \
-	"TCP_C4_S20" \
-	"TCP_C5_S21" \
-	"TCP_CO2_S22" \
-	"TCP_CO4_S23" \
-	"TCP_CO5_S24" \
-	"TCP_D2_S16" \
-	"TCP_D4_S17" \
-	"TCP_D5_S18" \
-	"TCP_M2_S13" \
-	"TCP_M4_S14" \
-	"TCP_M5_S15" \
-	"Undetermined_S0")
+for file in $workingdir/*
+do
+        if [[ $file == *R1.fastp ]]
+        then
+
+                files+=("$(basename ${file::-9})")
+
+        fi
+
+done
+
+echo $files
 
 # Map forward and reverse reads to indexed genome
 
 echo "RUNNING ALIGNMENT"
 
-for x in ${lane[@]}
+for file in ${files[@]}
 do
-	for i in ${list[@]}
-	do
-		echo ${i} " = running"
 
-		STAR \
-			--outMultimapperOrder Random \
-			--outSAMmultNmax 1 \
-			--runThreadN ${SLURM_CPUS_PER_TASK} \
-			--runMode alignReads \
-			--outSAMtype BAM Unsorted \
-			--quantMode GeneCounts \
-			--outFileNamePrefix ${i}_${x}_unsort. \
-			--genomeDir $refdir/ \
-			--readFilesIn /mnt/scratch/c1831460/RNA-seq/fastp/${i}_${x}.fastp1.gz \
-			$workingdir/fastp/${i}_${x}.fastp2.gz \
-			--readFilesCommand zcat
+        echo ${file} " = running"
 
-		echo ${i} " = complete"
-	done
+        STAR \
+                --outMultimapperOrder Random \
+                --outSAMmultNmax 1 \
+                --runThreadN ${SLURM_CPUS_PER_TASK} \
+                --runMode alignReads \
+                --outSAMtype BAM Unsorted \
+                --quantMode GeneCounts \
+                --outFileNamePrefix $exportdir/${file}_unsort. \
+                --genomeDir $refdir/ \
+                --readFilesIn $workingdir/${file}_R1.fastp \
+                $workingdir/${file}_R2.fastp
+
+        echo ${file} " = complete"
 done
 
 echo "ALIGNMENT COMPLETE"
