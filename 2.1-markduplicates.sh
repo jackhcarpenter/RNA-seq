900adf2f90f29f92e3d95513a680e61475dd1c1c
jackhcarpenter <carpenterj3@cardiff.ac.uk>
2024-06-28 09:47:34 +0100

    2.1-markduplicates.sh reusability
    The script now searches for input files in the STAR/ directory by extracting the basename of the *_unsort.Log.out files from 1.2-mapping.sh. The 2.2-merged_markduplicates file has been removed as it was part of a previous analysis that processed the lanes of each sample separately and then merged later on. As I already knew that the lanes could be merged, this processing merges fastq files in 0.1-lane_concat.sh

--- 2.1-markduplicates.sh
+++ 2.1-markduplicates.sh
@@ -3,9 +3,8 @@
 #SBATCH --partition=defq       # the requested queue
 #SBATCH --nodes=1              # number of nodes to use
 #SBATCH --tasks-per-node=1     #
-#SBATCH --cpus-per-task=4      #  
-#SBATCH --mem-per-cpu=8G       # in megabytes, unless unit explicitly stated
-#SBATCH --time=6:0:0
+#SBATCH --cpus-per-task=4      #
+#SBATCH --mem-per-cpu=16G       # in megabytes, unless unit explicitly stated
 #SBATCH --error=logs/%J.err         # redirect stderr to this file
 #SBATCH --output=logs/%J.out        # redirect stdout to this file
 #SBATCH --mail-user=carpenterj3@cardiff.ac.uk  # email
@@ -35,13 +34,20 @@ module load samtools/1.10
 
 # point to the directory containing the reference genome
 
-export refdir=/mnt/scratch/xxxxxx/RNA-seq/reference_genome
+export refdir=/mnt/scratch/xxxxxx/RNA-seq_TCP4_STM/reference_genome
+echo "Reference directory =" $refdir
 
 # define the working directory
 
-export workingdir=/mnt/scratch/xxxxxx/RNA-seq
+export workingdir=/mnt/scratch/xxxxxx/RNA-seq_TCP4_STM/STAR
+echo "Working directory =" $workingdir
 
-##REMEMBER: set up any directories that the software needs in this script in case 
+# define the export directory
+
+export "Export directory =" exportdir=/mnt/scratch/xxxxxxx/RNA-seq_TCP4_STM/markdup
+echo $exportdir
+
+##REMEMBER: set up any directories that the software needs in this script in case
 ##it is unable to do so itself
 
 #################################################################################
@@ -50,92 +56,72 @@ export workingdir=/mnt/scratch/xxxxxx/RNA-seq
 
 # Loop variables
 
-lane=("L001" \
-	"L002")
+declare -a files
 
-list=("STM_C3_S7" \
-	"STM_C4_S8" \
-	"STM_C5_S9" \
-	"STM_CD3_S10" \
-	"STM_CD4_S11" \
-	"STM_CD5_S12" \
-	"STM_D3_S4" \
-	"STM_D4_S5" \
-	"STM_D5_S6" \
-	"STM_M3_S1" \
-	"STM_M4_S2" \
-	"STM_M5_S3" \
-	"TCP_C2_S19" \
-	"TCP_C4_S20" \
-	"TCP_C5_S21" \
-	"TCP_CO2_S22" \
-	"TCP_CO4_S23" \
-	"TCP_CO5_S24" \
-	"TCP_D2_S16" \
-	"TCP_D4_S17" \
-	"TCP_D5_S18" \
-	"TCP_M2_S13" \
-	"TCP_M4_S14" \
-	"TCP_M5_S15" \
-	"Undetermined_S0")
+for file in $workingdir/*_unsort.Log.out
+do
+        echo ${file}
+        files+=("$(basename ${file::-15})")
 
-for x in ${lane[@]}
+done
+
+echo ${files}
+
+# Loops
+
+for file in ${files[@]}
 do
 
-	for i in ${list[@]}
+        echo "============================="
+        echo ${file} "samtoolsort = running"
 
-	do
-		
-		echo "============================="
-		echo ${i} "samtoolsort = running"
+        # Sort sequences so that they are organized by genomic coordinates
+        samtools sort \
+        -@ ${SLURM_CPUS_PER_TASK} \
+        -o $workingdir/${file}_sorted.bam \
+        $workingdir/${file}_unsort.Aligned.out.bam
 
-		# Sort sequences so that they are organized by genomic coordinates
-		samtools sort \
-		-@ ${SLURM_CPUS_PER_TASK} \
-		-o $workingdir/star/${i}_${x}.sorted.bam \
-		$workingdir/star/${i}_${x}_unsort.Aligned.out.bam
-		
-		samtools index \
-		$workingdir/star/${i}_${x}.sorted.bam
-		
-		echo ${i} "samtoolsort = complete"
-		echo "============================="
-		
-		echo ${i} "markdup = running"
+        samtools index \
+        $workingdir/${file}_sorted.bam
 
-		## Mark the duplicated reads
-		java -jar $PICARD MarkDuplicates \
-			I=$workingdir/star/${i}_${x}.sorted.bam \
-			O=$workingdir/markdup/${i}_${x}.markdup.bam \
-			M=$workingdir/markdup/${i}_${x}.metrics.markdup.txt \
-			REMOVE_DUPLICATES=false \
-			VALIDATION_STRINGENCY=SILENT
+        echo ${file} "samtoolsort = complete"
+        echo "============================="
 
-		bamtools stats \
-			-in $workingdir/markdup/${i}_${x}.markdup.bam \
-			> $workingdir/markdup/${i}_${x}.markdup.dupstats.txt
+        echo ${file} "markdup = running"
 
-		echo ${i} "markdup = complete"
+        ## Mark the duplicated reads
+        java -jar $PICARD MarkDuplicates \
+                I=$workingdir/${file}_sorted.bam \
+                O=$exportdir/${file}_markdup.bam \
+                M=$exportdir/${file}_metrics.markdup.txt \
+                REMOVE_DUPLICATES=false \
+                VALIDATION_STRINGENCY=SILENT
 
-		echo ${i} "remove duplicate = running"
+        bamtools stats \
+                -in $exportdir/${file}_markdup.bam \
+                > $exportdir/${file}_markdup_dupstats.txt
 
-		## Remove the duplicated reads
-		java -jar $PICARD MarkDuplicates \
-			I=$workingdir/star/${i}_${x}.sorted.bam \
-			O=$workingdir/markdup/${i}_${x}.rmdup.bam \
-			M=$workingdir/markdup/${i}_${x}.metrics.rmdup.txt \
-			REMOVE_DUPLICATES=true \
-			VALIDATION_STRINGENCY=SILENT
+        echo ${file} "markdup = complete"
 
-		bamtools stats \
-			-in $workingdir/markdup/${i}_${x}.rmdup.bam \
-			> $workingdir/markdup/${i}_${x}.rmdup.dupstats.txt
+        echo ${file} "remove duplicate = running"
 
-		echo ${i} "remove duplicate = complete"
-		echo "============================="
+        ## Remove the duplicated reads
+        java -jar $PICARD MarkDuplicates \
+                I=$workingdir/${file}_sorted.bam \
+                O=$exportdir/${file}_rmdup.bam \
+                M=$exportdir/${file}_metrics.rmdup.txt \
+                REMOVE_DUPLICATES=true \
+                VALIDATION_STRINGENCY=SILENT
+
+        bamtools stats \
+                -in $exportdir/${file}_rmdup.bam \
+                > $exportdir/${file}_rmdup.dupstats.txt
+
+        echo ${file} "remove duplicate = complete"
+        echo "============================="
 
 ## Now look at the files to see if it is better to keep or remove duplicates
-	done
+
 done
 
 #################################################################################
