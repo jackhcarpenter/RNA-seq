1379f40a661f8e87dc156516195f07c706095b08
jackhcarpenter <carpenterj3@cardiff.ac.uk>
2024-07-02 17:00:06 +0100

    4.1-DESeq.R
    Script adapted for use following previous commit's update to merge files based on time point. Also contains a loop for comparing both genotypes to the WT in dds.

--- 4.1-DESeq.R
+++ 4.1-DESeq.R
@@ -25,69 +25,108 @@ library("apeglm")
 
 # Point to the working dir
 
-setwd("C:/Users/c1831460/OneDrive - Cardiff University/Documents/DTP/Second Year/RNA-seq/featureCounts/")
+setwd("C:/Users/c1831460/OneDrive - Cardiff University/Documents/DTP/Second Year/RNA-seq TCP4_STM/")
 
-# Create dataframe contianing output from 3-featureCounts.sh
+# Create dataframes containing output from 3.2-featureCounts_merged.sh
 
-STM <- list.files(
-  pattern = "STM.*.csv")
+t3h <- list.files(
+  pattern = "3h.*.csv")
 
-TCP4 <- list.files(
-  pattern = "TCP4.*.csv")
+t12h <- list.files(
+  pattern = "12h.*.csv")
+
+t24h <- list.files(
+  pattern = "24h.*.csv")
+
+t11d <- list.files(
+  pattern = "11d.*.csv")
 
 # Create tables to tell DESeq2 what the variables are
 # The number of samplesheets (coldata) you need will depend on the number of
 # timepoints/variables you want to compare
 
-#STM
-STM_coldata <- data.frame(
-  sample = factor(c("STM_C3_S7",
-                    "STM_C4_S8",
-                    "STM_C5_S9",
-                    "STM_CD3_S10",
-                    "STM_CD4_S11",
-                    "STM_CD5_S12",
-                    "STM_D3_S4",
-                    "STM_D4_S5",
-                    "STM_D5_S6",
-                    "STM_M3_S1",
-                    "STM_M4_S2",
-                    "STM_M5_S3"
+#3h
+coldata_3h <- data.frame(
+  sample = factor(c("Col-0_3h_1",
+                    "Col-0_3h_2",
+                    "Col-0_3h_3",
+                    "STM_3h_1",
+                    "STM_3h_2",
+                    "STM_3h_3",
+                    "TCP_3h_1",
+                    "TCP_3h_2",
+                    "TCP_3h_3"
   )),
-  treatment = factor(c("C", "C", "C",
-                       "CD","CD","CD",
-                       "D","D","D",
-                       "M","M", "M"
+  treatment = factor(c("Col", "Col", "Col",
+                       "STM","STM","STM",
+                       "TCP","TCP","TCP"
   )),
   replicate = factor(c("1", "2", "3",
-                       "1", "2", "3",
                        "1", "2", "3",
                        "1", "2", "3")
   ))
 
 
-#TCP4
-TCP4_coldata <- data.frame(
-  sample = factor(c("TCP_C2_S19",
-                    "TCP_C4_S20",
-                    "TCP_C5_S21",
-                    "TCP_CD2_S22",
-                    "TCP_CD3_S23",
-                    "TCP_CD5_S24",
-                    "TCP_D2_S16",
-                    "TCP_D4_S17",
-                    "TCP_D5_S18",
-                    "TCP_M2_S13",
-                    "TCP_M4_S14",
-                    "TCP_M5_S15"
+#12h
+coldata_12h <- data.frame(
+  sample = factor(c("Col-0_12h_1",
+                    "Col-0_12h_2",
+                    "Col-0_12h_4",
+                    "STM_12h_1",
+                    "STM_12h_2",
+                    "STM_12h_3",
+                    "TCP_12h_1",
+                    "TCP_12h_2",
+                    "TCP_12h_4"
   )),
-  treatment = factor(c("C", "C", "C",
-                       "CD","CD","CD",
-                       "D","D","D",
-                       "M","M", "M"
+  treatment = factor(c("Col", "Col", "Col",
+                       "STM","STM","STM",
+                       "TCP","TCP","TCP"
   )),
   replicate = factor(c("1", "2", "3",
                        "1", "2", "3",
+                       "1", "2", "3")
+  ))
+
+
+#24h
+coldata_24h <- data.frame(
+  sample = factor(c("Col-0_24h_1",
+                    "Col-0_24h_3",
+                    "Col-0_24h_4",
+                    "STM_24h_2",
+                    "STM_24h_3",
+                    "STM_24h_5",
+                    "TCP_24h_1",
+                    "TCP_24h_3",
+                    "TCP_24h_4"
+  )),
+  treatment = factor(c("Col", "Col", "Col",
+                       "STM","STM","STM",
+                       "TCP","TCP","TCP"
+  )),
+  replicate = factor(c("1", "2", "3",
+                       "1", "2", "3",
+                       "1", "2", "3")
+  ))
+
+#11d
+coldata_11d <- data.frame(
+  sample = factor(c("Col-0_11d_1",
+                    "Col-0_11d_2",
+                    "Col-0_11d_3",
+                    "STM_11d_1",
+                    "STM_11d_2",
+                    "STM_11d_3",
+                    "TCP_11d_1",
+                    "TCP_11d_2",
+                    "TCP4_11d_5"
+  )),
+  treatment = factor(c("Col", "Col", "Col",
+                       "STM","STM","STM",
+                       "TCP","TCP","TCP"
+  )),
+  replicate = factor(c("1", "2", "3",
                        "1", "2", "3",
                        "1", "2", "3")
   ))
@@ -98,23 +137,29 @@ TCP4_coldata <- data.frame(
 
 # Create loop and var for loop
 
-# List of geneotypes to initiate the for loop
-gene = list(STM = STM, TCP4 = TCP4)
+# List of timepoints to initiate the for loop
+gene = list(t3h = t3h, t12h = t12h, t24h = t24h, t11d = t11d)
 
-# For passing different genotype data into coldata 
-colDatafield <- list(STM_coldata, TCP4_coldata)
+# For passing different timepoint data into coldata 
+colDatafield <- list(coldata_3h, coldata_12h, coldata_24h, coldata_11d)
 
 # Markduplicates and removeduplicates name list
 namefield <- c("markdup", "rmdup")
 
-# List of geneotype names as factor
-genotypefield <- c("STM", "TCP4")
+# List of timepoint names as factor
+timepointfield <- c("3h", "12h", "24h", "11d")
+
+# List of genotypes as a factor
+genotypefield <- c("STM","TCP")
 
 # To switch between namefield variables
 index1 = as.numeric(1)
 
-# To switch between genotype fields
+# To switch between timepoint fields
 index2 = as.numeric(1)
+
+# To switch between genotype fields
+index3 = as.numeric(1)
 
 
 for (i in gene) {
@@ -157,7 +202,7 @@ for (i in gene) {
   
     dds$treatment <- relevel(
       dds$treatment, 
-      ref = "C")
+      ref = "Col")
   
   # Run the DESeq command on the DESeq dataset
   
@@ -166,103 +211,113 @@ for (i in gene) {
   # Look at the contrasts we can build
     resultsNames(dds)
     
-# Generate a results table, and specify the contrast we want to build
-  
-    res <- results(
-      dds,
-      contrast=c(
-        "treatment",
-        "CD",
-        "C"))
-  
-  #OR...
+  # Generate a results table, and specify the contrast we want to build
     
-#    res <- results(
-#      dds,
-#      name = "treatment_M_vs_D"
-#    )
-
-  # Log fold change shrinkage for visualisation can be done using different models
-  
-    resLFC <- lfcShrink(
-      dds,
-      coef= "treatment_CD_vs_C",
-      type= "apeglm")
-  
-    resLFC
-  
-  #OR...
-  
-#    resNorm <- lfcShrink(
-#      dds, 
-#      coef = "genotype__vs_", 
-#      type = "normal")
+    index3 = as.numeric(1)
     
-#    resNorm
+    for(x in genotypefield) {
   
-  # Plotting the normalised results with MA
-  # Set the probability and log fold change thresholds
-  
-    xlim <- c(1,1e5); ylim <- c(-3,3)
-    plotMA(
-      resLFC, 
-      xlim=xlim, 
-      ylim=ylim, 
-      main="apeglm")
-  
-  # The plot can be used to identify the rownumber of individual genes
-  # interactively
-  
-  #  idx <- identify(
-  #    res$baseMean, 
-  #    res$log2FoldChange)
+      res <- results(
+        dds,
+        contrast=c(
+          "treatment",
+          genotypefield[index3],
+          "Col"))
     
-  #  rownames(res)[idx]
-  
-  # Extract all genes (independent of differential expression)
-    print(file.path("DEGs", genotypefield[index2],
-      paste( 
-      genotypefield[index2], "_C_vs_CD_", namefield[index1], "_DEGs.csv", 
-      sep = "")))
-    
-    write.csv(resLFC, (
-      file = file.path("DEGs", genotypefield[index2],
-        paste( 
-        genotypefield[index2], "_C_vs_CD_", namefield[index1], "_DEGs.csv", 
-        sep = ""))))
-  
-  # Extract significant upregulated and down regulated genes into separate
-  # datasets
-  
-    upreg <- subset(
-      resLFC, 
-      log2FoldChange >=0.5 & padj <0.05)
-    
-    upreg <- upreg[ , c(-1,-3)]
-  
-    downreg <-subset(
-      resLFC, 
-      log2FoldChange <=-0.5 & padj <0.05)
-    
-    downreg <- downreg[ , c(-1,-3)]
-  
-  #write csv file of for up and down regualted gene IDs
-  
-    write.csv(upreg, (
-      file = file.path("DEGs", genotypefield[index2],
-                       paste( 
-                         genotypefield[index2], "_C_vs_CD_", namefield[index1], 
-                         "_upreg.csv", 
-                         sep = "")))
-      )
+    #OR...
       
-    write.csv(downreg, (
-      file = file.path("DEGs", genotypefield[index2],
-                       paste( 
-                         genotypefield[index2], "_C_vs_CD_", namefield[index1], 
-                         "_downreg.csv", 
-                         sep = "")))
-      )
+    #    res <- results(
+    #      dds,
+    #      name = "treatment_M_vs_D"
+    #    )
+  
+    # Log fold change shrinkage for visualisation can be done using different models
+      
+      coef <- (paste0("treatment_", genotypefield[index3], "_vs_Col"))
+    
+      resLFC <- lfcShrink(
+        dds,
+        coef= coef,
+        type= "apeglm")
+    
+      resLFC
+    
+    #OR...
+    
+    #    resNorm <- lfcShrink(
+    #      dds, 
+    #      coef = "timepoint__vs_", 
+    #      type = "normal")
+      
+    #    resNorm
+    
+    # Plotting the normalised results with MA
+    # Set the probability and log fold change thresholds
+    
+      xlim <- c(1,1e5); ylim <- c(-3,3)
+      plotMA(
+        resLFC, 
+        xlim=xlim, 
+        ylim=ylim, 
+        main="apeglm")
+    
+    # The plot can be used to identify the rownumber of individual genes
+    # interactively
+    
+    #  idx <- identify(
+    #    res$baseMean, 
+    #    res$log2FoldChange)
+      
+    #  rownames(res)[idx]
+    
+    # Extract all genes (independent of differential expression)
+      print(file.path("DEGs", timepointfield[index2],
+        paste( 
+        timepointfield[index2], "_Col_vs_", genotypefield[index3], "_", namefield[index1], "_DEGs.csv", 
+        sep = "")))
+      
+      write.csv(resLFC, (
+        file = file.path("DEGs", timepointfield[index2],
+          paste( 
+          timepointfield[index2], "_Col_vs_", genotypefield[index3], "_", namefield[index1], "_DEGs.csv", 
+          sep = ""))))
+    
+    # Extract significant upregulated and down regulated genes into separate
+    # datasets
+    
+      upreg <- subset(
+        resLFC, 
+        log2FoldChange >=0.5 & padj <0.05)
+      
+      upreg <- upreg[ , c(-1,-3)]
+    
+      downreg <-subset(
+        resLFC, 
+        log2FoldChange <=-0.5 & padj <0.05)
+      
+      downreg <- downreg[ , c(-1,-3)]
+    
+    #write csv file of for up and down regualted gene IDs
+    
+      write.csv(upreg, (
+        file = file.path("DEGs", timepointfield[index2],
+                         paste( 
+                           timepointfield[index2], "_Col_vs_", genotypefield[index3], "_", namefield[index1], 
+                           "_upreg.csv", 
+                           sep = "")))
+        )
+        
+      write.csv(downreg, (
+        file = file.path("DEGs", timepointfield[index2],
+                         paste( 
+                           timepointfield[index2], "_Col_vs_", genotypefield[index3], "_", namefield[index1], 
+                           "_downreg.csv", 
+                           sep = "")))
+        )
+      
+      index3 <- index3+1
+    
+    }
     
     index1 <- index1+1
   
